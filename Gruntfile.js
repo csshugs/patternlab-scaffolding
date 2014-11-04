2ec5a837bf3a90ab2d226d5eb7da4b7131663478
Dennis Heibült <dennis@csshugs.com>
2014-11-04 11:24:44 +0100

    add concatination and uglification for cms dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,11 @@ module.exports = function(grunt) {
         watch: {
             scss: {
                 files: ['source/css/**/{.*,*,*/*}'],
-                tasks: 'sass'
+                tasks: 'scss'
+            },
+            js: {
+                files: ['source/js/**/{.*,*,*/*}'],
+                tasks: 'js'
             },
             livereload: {
                 options: {
@@ -42,25 +46,84 @@ module.exports = function(grunt) {
             ]
         },
         sass: {
-            dist: {
+            options: {
+                require: 'sass-globbing',
+                loadPath: 'bower_components'
+            },
+            dev: {
                 options: {
-                    require: 'sass-globbing',
-                    loadPath: 'bower_components',
                     style: 'expanded',
                     lineNumbers: true
                 },
                 files: {
                     'public/css/style.css': 'source/css/style.scss'
                 }
+            },
+            cms: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'cms/css/style.css': 'source/css/style.scss'
+                }
+            }
+        },
+        autoprefixer: {
+            options: {
+                browsers: [
+                    'last 3 version',
+                    'ie 8',
+                    'ie 9',
+                    'ie 10'
+                ]
+            },
+            cms: {
+                src: 'cms/css/style.css'
+            }
+        },
+        concat: {
+            options: {
+                separator: ' ',
+            },
+            plugins: {
+                src: ['source/js/plugins/*.js'],
+                dest: 'public/js/plugins.js',
+            },
+            cms: {
+                src: ['source/js/plugins/*.js', 'source/js/script.js'],
+                dest: 'cms/js/script.js',
+            }
+        },
+        uglify: {
+            cms: {
+                files: {
+                    'cms/js/script.js': 'cms/js/script.js'
+                }
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('scss', [
+        'sass',
+        'autoprefixer'
+    ]);
+
+    grunt.registerTask('js', [
+        'concat',
+        'uglify'
+    ]);
+
     grunt.registerTask('default', [
+        'concat',
+        'uglify',
         'concurrent',
         'connect:server',
         'watch'
